6fd7ee143903bada6abb02ebd300ff3a20a1ebb7
LeonardoSbardelotto <60637969+LeonardoSbardelotto@users.noreply.github.com>
2020-02-04 19:05:15 -0800

    Running app

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,41 @@
+import 'package:calculadora_cod3r/models/memory.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../components/display.dart';
+import '../components/keyboard.dart';
+
+class Calculator extends StatefulWidget {
+  onPressed(String text) {
+    print(text);
+  }
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+
+    return MaterialApp(
+      home: Column(
+        children: <Widget>[
+          Display(memory.value),
+          Keyboard(_onPressed),
+        ],
+      ),
+    );
+  }
+}
